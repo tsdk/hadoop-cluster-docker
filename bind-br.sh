65aa6a34aff7fe7350e125e37d51981663303e10
tsdk <st_zzh@sina.com>
2019-04-25 02:36:34 +0800

    add tool scripts

---
+++ bind-br.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# only run one time for your machine
+if [ `whoami` != "root" ];then
+echo "sudo $0 $@"
+exit
+fi
+
+# the default bridge is br0
+N=${1:-0}
+# your gateway
+gw="192.168.82.2"
+# your localIP/mask
+net="192.168.82.100/24"
+
+ip addr add $net dev br$N; \
+ip addr del $net dev eth0; \
+brctl addif br$N eth0; \
+ip route del default; \
+ip route add default via $gw dev br$N
+
---
+++ bind-ip.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ `whoami` != "root" ];then
+echo "sudo $0 $@"
+exit
+fi
+
+
+# the default node number is 3 
+N=${1:-3}
+gw="192.168.82.2"
+
+hosts="${gw}00 hadoop-master"
+i=1
+while [ $i -lt $N ]
+do
+    hosts="$hosts\n${gw}0$i hadoop-slave$i"
+    i=$(( $i + 1 ))
+done
+
+# start hadoop master container
+pipework br0 hadoop-master ${gw}00/24@$gw
+docker exec -it hadoop-master bash -c "echo \"\$(sed 's/^.*master.com$/${hosts}/' /etc/hosts)\" > /etc/hosts"
+
+# start hadoop slave container
+i=1
+while [ $i -lt $N ]
+do
+    pipework br0 hadoop-slave$i ${gw}0$i/24@$gw
+    docker exec -it hadoop-slave$i bash -c "echo \"\$(sed 's/^.*slave$i.com$/${hosts}/' /etc/hosts)\" > /etc/hosts"
+	i=$(( $i + 1 ))
+done 
+
+docker exec -it hadoop-master bash -c "/root/start-hadoop.sh"
